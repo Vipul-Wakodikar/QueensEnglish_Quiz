b5915c14a6bdd677827cd41309e99be70602a2a1
Vipul-Wakodikar <wakodikar.vipul@gmail.com>
2021-11-24 21:43:25 +0530

    final commit

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,12 +1,18 @@
-import "../App.css";
+import AppCSS from "../App.module.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 const Main = () =>{
+    //Loading is use to display loading screen 
     const [isLoading, setIsLoading] = useState(false);
+    //setContent is for displaying question
     const [content, setContent] = useState("");
+    //Displaying answers
     const [answer, setAnswer] = useState("");
+    //Check if the answer provided by user is correct
     const [answerStats,setAnswerStats] = useState(false);
+    //check if the button is clicked 
     const [isSubmitted,setIsSubmitted] = useState(false);
+    //use to change the question by reloading api
     const [loader,setLoader] = useState(false);
     const [texter,setText] = useState("");
     const [question,setQuestion] = useState("Please click on submit to start and wait for 3 seconds");
@@ -38,6 +44,7 @@ const Main = () =>{
     //Behaviour after clicking on submit 
     const checkHandler = () =>{
       setCount(count+1);
+      //clear text and wait for x seconds before displaying new question
       setTimeout(()=>{
         if(!loader ? setLoader(true):setLoader(false));
         if(isSubmitted)setIsSubmitted(false);
@@ -77,9 +84,12 @@ const Main = () =>{
         <div></div>
         <p>{question}</p>
         <textarea placeholder="Enter your Answer" onChange = {onAnswerHandler}/>
-        <div><button onClick = {checkHandler} id = "submitButton">Submit</button></div>
+        <div><button onClick = {checkHandler} class = {AppCSS.btnCSS}>Submit</button></div>
+        {/*Display if answer is correct*/}
         {isSubmitted && answerStats ? (<h3>{texter}</h3>):(<h3></h3>)}
+        {/*Display if answer is Wrong--------------*/}
         {isSubmitted && !answerStats && count>1 ? (<h3>{texter}</h3>):(<h3></h3>)}
+        {/*Home Screen Content-------------------- */}
         {count === 0? (<div></div>):(<div></div>)}
         </>
         )}  
